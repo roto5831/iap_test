d35610bf7b06b717ea29cd423cf821d8512b46e3
Hirotomo Kobayashi <hiro-kobayashi@ixit.co.jp>
2018-06-28 13:41:32 +0900

    消費型

---
+++ IAP_TEST/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IAP_TEST
+//
+//  Created by 小林 宏知 on 2018/06/28.
+//  Copyright © 2018年 小林 宏知. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
